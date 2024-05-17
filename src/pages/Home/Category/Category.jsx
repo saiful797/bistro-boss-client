df8572a7d33bf3ea85e416b2037826b5d4162dc7
saiful797 <saifulislamsaif797@gmail.com>
2024-05-17 11:56:33 +0600

    Added sectionTitle component for uses section title

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -8,40 +8,48 @@ import slider2 from '../../../assets/home/slide2.jpg';
 import slider3 from '../../../assets/home/slide3.jpg';
 import slider4 from '../../../assets/home/slide4.jpg';
 import slider5 from '../../../assets/home/slide5.jpg';
+import SectionTitle from '../../../components/SectionTitle';
 
 const Category = () => {
     return (
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img className='mx-auto' src={slider1} alt="" />
-          <h1 className='text-4xl uppercase text-center -mt-16 text-white'>Salads</h1>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='mx-auto' src={slider2} alt="" />
-          <h1 className='text-4xl uppercase text-center -mt-16 text-white'>Pizza</h1>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='mx-auto' src={slider3} alt="" />
-          <h1 className='text-4xl uppercase text-center -mt-16 text-white'>Salads</h1>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='mx-auto' src={slider4} alt="" />
-          <h1 className='text-4xl uppercase text-center -mt-16 text-white'>Salads</h1>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='mx-auto' src={slider5} alt="" />
-          <h1 className='text-4xl uppercase text-center -mt-16 text-white'>Salads</h1>
-        </SwiperSlide>
-        
-      </Swiper>
+      <section>
+        <SectionTitle
+          subHeading={'From 11.00 AM to 10.00 PM'}
+          heading={'Order Online'}
+        >
+            
+        </SectionTitle>
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img className='mx-auto' src={slider1} alt="" />
+            <h1 className='text-4xl uppercase text-center -mt-16 text-white'>Salads</h1>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className='mx-auto' src={slider2} alt="" />
+            <h1 className='text-4xl uppercase text-center -mt-16 text-white'>Pizza</h1>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className='mx-auto' src={slider3} alt="" />
+            <h1 className='text-4xl uppercase text-center -mt-16 text-white'>Salads</h1>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className='mx-auto' src={slider4} alt="" />
+            <h1 className='text-4xl uppercase text-center -mt-16 text-white'>Salads</h1>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className='mx-auto' src={slider5} alt="" />
+            <h1 className='text-4xl uppercase text-center -mt-16 text-white'>Salads</h1>
+          </SwiperSlide>
+        </Swiper>
+      </section>
     );
 };
 
