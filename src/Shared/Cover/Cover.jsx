4562fc46a73d4349ba08194518c16e43cfd05cb1
saiful797 <saifulislamsaif797@gmail.com>
2024-05-18 21:37:20 +0600

    Updated menuCategory

--- src/Shared/Cover/Cover.jsx
+++ src/Shared/Cover/Cover.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import { Parallax} from 'react-parallax';
 
 
-const Cover = ({ img }) => {
+const Cover = ({ img, title }) => {
   return (
     <Parallax
         blur={{ min: -50, max: 50 }}
@@ -16,7 +16,7 @@ const Cover = ({ img }) => {
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Our Menu</h1>
+                    <h1 className="mb-5 text-5xl font-bold">{title}</h1>
                     <p className="mb-5">
                         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
                         excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
@@ -31,5 +31,6 @@ const Cover = ({ img }) => {
 
 Cover.propTypes ={
     img: PropTypes.any,
+    title: PropTypes.string,
 }
 export default Cover;
